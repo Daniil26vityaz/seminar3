7a05dee002cea7c3ebf3df87241ea8574183b26c
Daniil <mr.varfolomeev@bk.ru>
2023-07-25 15:46:26 +0300

    ended task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,11 +6,19 @@
 
 
 Console.WriteLine("Введите пятизначное число: ");
-int[]array = number[5];
-if(number < 100000 && number > -100000)
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100000 && number > -100000)
 {
-
+    if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+    {
+        Console.WriteLine("Введеное число палиндром! ");
+    }
+    else
+    {
+        Console.WriteLine("Введеное число не палиндром! ");
+    }
 }
-else{
+else
+{
     Console.WriteLine("Введеное число неверно! ");
 }
--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca416d4c9dba7a3d85750b96d641a50cf16436f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2279419c4a636a909e2def2292f7543ff67c60ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
