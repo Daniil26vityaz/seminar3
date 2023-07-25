3a89ff320aa4a4e2e7847dacd1c94feb7443d389
Daniil <mr.varfolomeev@bk.ru>
2023-07-25 16:20:00 +0300

    ended task2

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,3 +3,29 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task3()
+{
+    int number = Input("Введите положительное число: ");
+    int counter = 1;
+    if (number > 0)
+    {
+        while (counter <= number)
+        {
+            Console.WriteLine($"куб числа {counter} = {Math.Pow(counter, 3)}");
+            counter++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введенное число меньше или равно нулю");
+    }
+
+}
+Console.Clear();
+Task3();
